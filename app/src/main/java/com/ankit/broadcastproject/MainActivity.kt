c644e33470e96a7da12c155893b5e612025bad37
ryanwammons <ryan@ammonsdatasolutions.com>
2020-05-05 18:21:18 -0400

    Fixes (hopefully will build)

--- app/src/main/java/com/ankit/broadcastproject/MainActivity.kt
+++ app/src/main/java/com/ankit/broadcastproject/MainActivity.kt
@@ -3,8 +3,6 @@ package com.ankit.broadcastproject
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -20,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     private val APPLICATION_ID = "PLEASE INSERT YOUR APPLICATION SPECIFIC ID PROVIDED BY BAMBUSER"
 
     private val broadcaster by lazy {
-        Broadcast(this, APPLICATION_ID, broadcasterObserver)
+        Broadcaster(this, APPLICATION_ID, broadcasterObserver)
     }
 
     override fun onPause() {
@@ -45,26 +43,27 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         broadcaster.onActivityDestroy()
     }
+
     private val broadcasterObserver = object : Broadcaster.Observer {
-        fun onConnectionStatusChange(broadcastStatus: BroadcastStatus) {
+        override fun  onConnectionStatusChange(broadcastStatus: BroadcastStatus) {
             Log.i("Mybroadcastingapp", "Received status change: $broadcastStatus")
         }
-        fun onStreamHealthUpdate(i: Int) {}
-        fun onConnectionError(connectionError: ConnectionError, s: String?) {
+        override fun onStreamHealthUpdate(i: Int) {}
+        override fun onConnectionError(connectionError: ConnectionError, s: String?) {
             Log.i("Mybroadcastingapp","Received connection error: $connectionError, $s")
         }
-        fun onCameraError(cameraError: CameraError) {
+        override fun onCameraError(cameraError: CameraError) {
             Log.i("Mybroadcastingapp","Received camera error: $cameraError")
         }
-        fun onChatMessage(s: String) {
+        override fun onChatMessage(s: String) {
             Log.i("Mybroadcastingapp","Received chat messsage: $s")
         }
-        fun onResolutionsScanned() {}
-        fun onCameraPreviewStateChanged() {}
-        fun onBroadcastInfoAvailable(s: String, s1: String) {
+        override fun onResolutionsScanned() {}
+        override fun onCameraPreviewStateChanged() {}
+        override fun onBroadcastInfoAvailable(s: String, s1: String) {
             Log.i("Mybroadcastingapp","Received broadcast info: $s, $s1")
         }
-        fun onBroadcastIdAvailable(id: String) {
+        override fun onBroadcastIdAvailable(id: String) {
             Log.i("Mybroadcastingapp","Received broadcast id: $id")
         }
     }
